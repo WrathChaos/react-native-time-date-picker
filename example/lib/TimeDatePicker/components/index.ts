86e05159d9b27e3b3269a6fa7d164d3496f8fa56
FreakyCoder <kurayogun@gmail.com>
2022-09-28 16:51:33 +0300

    chore: example is updated

--- example/lib/TimeDatePicker/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./Header/Header";
-export * from "./Days/Days";
-export * from "./SelectMonth/SelectMonth";
-export * from "./Calendar/Calendar";
-export * from "./SelectTime/SelectTime";
